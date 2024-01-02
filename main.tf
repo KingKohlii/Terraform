fada96e5774981fd5d5e950509507837582999f5
mayuravhad333@gmail.com <mayuravhad333@gmail.com>
2024-01-02 09:40:47 +0000

    commit

--- main.tf
+++ main.tf
@@ -1,23 +1,38 @@
-terraform {
-  required_version = "~> 1.1"
-  required_providers {
-    aws = {
-      version = "~> 3.27"
-    }
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
+variable "region_name" {
+  type    = string
+  default = "ap-south-1"
+
+  validation {
+    condition      = can(regex("^(ap-south-1|us-east-1)$", var.region_name))
+    error_message = "valid regions are Mumbai and N Virginia only"
+
   }
+
 }
 
 provider "aws" {
-
-  region = "ap-south-1"
+  region     = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
 
 }
 
 resource "aws_instance" "myec2" {
-  count = 3
+
   ami           = "ami-0a0f1259dd1c90938"
   instance_type = "t2.micro"
-tags = {
-Name = "tagwala-${count.index+1}"
 }
+ 
+ output "public_ip" {
+
+value= aws_instance.myec2.public_ip
+
 }
